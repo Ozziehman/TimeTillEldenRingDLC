3b9d5a0a78943c3622eabd7c50e130da33aa1950
Oscar Theelen <oscartheelen@gmail.com>
2024-02-23 03:32:27 +0100

    final full

--- static/script.js
+++ static/script.js
@@ -1,7 +1,12 @@
-var imagePaths = ['./static/images/FireBoss.png','./static/images/HippoBoss.png','./static/images/HorseRunning.png']
+var imagePaths = ['./static/images/FireBoss.png','./static/images/HippoBoss.png','./static/images/HorseRunning.png',
+'./static/images/LionBoss.png','./static/images/LucariaMaybe.png','./static/images/Miquella.png',
+'./static/images/MohgArena.png','./static/images/OldGuy.png','./static/images/OpenWorld.png',
+'./static/images/Painting.png','./static/images/PotDungeon.png','./static/images/StormVeilMaybe.png','./static/images/TreeVeil.png'
+]
 
 window.addEventListener('load', () => {
     var randomIndex = Math.floor(Math.random() * imagePaths.length);
     var randomImagePath = imagePaths[randomIndex];
     document.body.style.backgroundImage = `url(${randomImagePath})`;
+    document.body.style.backgroundSize = "cover"
 })
